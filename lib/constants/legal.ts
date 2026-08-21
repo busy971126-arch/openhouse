bb2e2623852c54b28dbbda47ee21fe497df5c121
busy971126-arch <busy971126@gmail.com>
2026-08-21 20:42:38 +0900

    feat(auth): define legal consent versions

---
+++ lib/constants/legal.ts
@@ -0,0 +1,10 @@
+export const TERMS_VERSION = "2026-08-21-beta";
+export const PRIVACY_VERSION = "2026-08-21-beta";
+
+export const LEGAL_EFFECTIVE_DATE = "2026년 8월 21일";
+
+export type ConsentSource =
+  | "signup_email"
+  | "onboarding_social"
+  | "reauthorization"
+  | "settings";
